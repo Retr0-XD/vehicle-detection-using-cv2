30ebcdcdfe9f65e9ad473ef2b8aa192cfb9cde7d
Retr0-XD <sakthiharish705@gmail.com>
2022-11-03 00:32:13 +0530

    cc

---
+++ object_tracking/main.py
@@ -0,0 +1,52 @@
+import cv2
+from tracker import *
+
+
+tracker = EuclideanDistTracker()
+
+#cap = cv2.VideoCapture("nigger.mp4")
+cap = cv2.VideoCapture("videoplayback.mp4")
+
+
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+
+while True:
+    ret, frame = cap.read()
+    height, width, _ = frame.shape
+    #print(height , width)
+
+  
+    roi = frame[0: 720,159: 348]
+
+   
+    ghost = object_detector.apply(roi)
+    _, ghost = cv2.threshold(ghost, 254, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(ghost, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
+    for cnt in contours:
+      
+        area = cv2.contourArea(cnt)
+        if area > 100:
+           
+            x, y, w, h = cv2.boundingRect(cnt)
+
+
+            detections.append([x, y, w, h])
+
+    
+    boxes_ids = tracker.update(detections)
+    for box_id in boxes_ids:
+        x, y, w, h, id = box_id
+        cv2.putText(roi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow("roi", roi)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("ghost_mode", ghost)
+
+    key = cv2.waitKey(30)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
